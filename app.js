a9b0de914806eea9d77d9a5f7940a5aeaaae8602
azatmagdeev <azatmagdeev@gmail.com>
2021-03-08 03:06:09 +0300

    typescript added

--- app.js
+++ app.js
@@ -1,62 +1,57 @@
-const lower_cases = 'abcdefghijklmnopqrstuvwxyz';
-const upper_cases = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-const numbers = '0123456789';
-const symbols = '!@#$%^&*(){}[]_-+=?><:;~';
-let string = '';
-const resultEl = document.querySelector('#result');
-const form = document.querySelector('#form');
-
+var lower_cases = 'abcdefghijklmnopqrstuvwxyz';
+var upper_cases = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var numbers = '0123456789';
+var symbols = '!@#$%^&*(){}[]_-+=?><:;~';
+var string = '';
+var resultEl = document.querySelector('#result');
+var form = document.querySelector('#form');
 function generateString() {
     string = '';
-    form.lower.checked ? string += lower_cases : null;
-    form.upper.checked ? string += upper_cases : null;
-    form.num.checked ? string += numbers : null;
-    form.symb.checked ? string += symbols : null;
-    return string
+    form['lower'].checked ? string += lower_cases : null;
+    form['upper'].checked ? string += upper_cases : null;
+    form['num'].checked ? string += numbers : null;
+    form['symb'].checked ? string += symbols : null;
+    return string;
 }
-
-form.addEventListener('change', () => {
+form.addEventListener('change', function () {
     generateString();
-    !string ? document.querySelector('#form button').classList.add('disabled') : document.querySelector('#form button').classList.remove('disabled')
+    !string ? document.querySelector('#form button').classList.add('disabled') : document.querySelector('#form button').classList.remove('disabled');
 });
-
-form.addEventListener('submit', (e) => {
+form.addEventListener('submit', function (e) {
     e.preventDefault();
     generateString();
-    const passwords = [];
-    for (let i = 0; i < 8; i++) {
-        passwords.push(getPassString())
+    var passwords = [];
+    for (var i = 0; i < 8; i++) {
+        passwords.push(getPassString());
     }
-    renderPasswords(passwords)
+    renderPasswords(passwords);
 });
-
 function getPassString() {
-    let pass_string = '';
-    for (let i = 0; i < Number(form.pass_length.value); i++) {
-        const position = Math.floor(Math.random() * string.length);
-        pass_string += string[position]
+    var pass_string = '';
+    for (var i = 0; i < Number(form['pass_length'].value); i++) {
+        var position = Math.floor(Math.random() * string.length);
+        pass_string += string[position];
     }
-    return pass_string
+    return pass_string;
 }
-
 function renderPasswords(passwords) {
     resultEl.innerHTML = '';
-    passwords.map(el => {
-        const p = document.createElement('p');
-        const span = document.createElement('span');
+    passwords.map(function (el) {
+        var p = document.createElement('p');
+        var span = document.createElement('span');
         span.textContent = el;
-        const copy_icon = document.createElement('i');
+        var copy_icon = document.createElement('i');
         copy_icon.textContent = 'content_copy';
         copy_icon.className = 'material-icons';
         p.append(span, copy_icon);
         resultEl.append(p);
-        copy_icon.addEventListener('click', async () => {
-            await navigator.clipboard.writeText(el);
-            document.querySelectorAll('.material-icons').forEach(el=>{
-                console.log('hello');
-                el.textContent = 'content_copy'
+        copy_icon.addEventListener('click', function () {
+            navigator.clipboard.writeText(el).then(function () {
+                document.querySelectorAll('.material-icons').forEach(function (el) {
+                    el.textContent = 'content_copy';
+                });
+                copy_icon.textContent = 'check';
             });
-            copy_icon.textContent = 'check'
-        })
-    })
+        });
+    });
 }
