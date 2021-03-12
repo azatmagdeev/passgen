ee4b73b3f26f2e23edddf57fe1a70a3563f1a6f0
azatmagdeev <azatmagdeev@gmail.com>
2021-03-12 05:38:06 +0300

    trying useState

--- passgen-react/src/components/Form.js
+++ passgen-react/src/components/Form.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Checkbox from "./Checkbox";
 import Radio from "./Radio";
 
@@ -50,23 +50,29 @@ function Form() {
         },
     ]
 
+    console.log(radios)
 
-    function changedForm() {
+    function handleFormChange(event) {
         console.log('form changed!')
+        set
     }
 
-    function submittedForm(e) {
+    const [length, setLength] = useState(radios)
+
+
+
+    function handleFormSubmit(e) {
         e.preventDefault()
     }
 
     return (
-        <form onChange={changedForm} onSubmit={submittedForm}>
+        <form onChange={handleFormChange} onSubmit={handleFormSubmit}>
             <div className="form-body">
                 <div>
                     {checkboxes.map(o => <Checkbox key={o.name} checkbox={o}/>)}
                 </div>
                 <div>
-                    {radios.map(o => <Radio key={o.value} radio={o}/>)}
+                    {radios.map(o => <Radio key={o.value} radio={o} setLength={setLength}/>)}
                 </div>
             </div>
             <div className="form-footer">
@@ -78,3 +84,12 @@ function Form() {
 }
 
 export default Form;
+
+function generatePassword(string, length) {
+    let password = '';
+    for (let i = 0; i < length; i++) {
+        const position = Math.floor(Math.random() * string.length);
+        password += string[position]
+    }
+    return password
+}
--- passgen-react/src/components/Radio.js
+++ passgen-react/src/components/Radio.js
@@ -1,13 +1,13 @@
 import React from 'react'
 
 export default function Radio(props) {
-    const {radio} = props;
+    const {radio,setLength} = props;
     return (
         <p>
             <label>
                 <input name="pass_length" type="radio"
                        value={radio.value}
-                       defaultChecked={radio.checked}/>
+                       defaultChecked={radio.checked} />
                 <span>{radio.info}</span>
             </label>
         </p>
